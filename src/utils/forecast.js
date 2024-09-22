123adc39f722c39279512375fd4dc79057ccfb96
Baba Prem Prakash <152669128+premcodeexplorer@users.noreply.github.com>
2024-09-22 08:10:27 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,22 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-const url = 'http://api.weatherstack.com/current?access_key=' + process.env.WEATHERSTACK_API_KEY + '&query=' + latitude + ',' + longitude + '&units=f'
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=${latitude},${longitude}&units=f`
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
+            console.error('Weather service connection error:', error);
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
+            console.error('Weather service error:', body.error);
             callback('Unable to find location', undefined)
+        } else if (!body.current) {
+            console.error('Unexpected weather service response:', body);
+            callback('Unexpected response from weather service', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            const weatherDescription = body.current.weather_descriptions[0] || 'No description available';
+            const temperature = body.current.temperature;
+            callback(undefined, `${weatherDescription}. It is currently ${temperature} degrees out.`)
         }
     })
 }
