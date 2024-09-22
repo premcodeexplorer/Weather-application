1e7d98ebef50c93eb0f77894365086c4967342af
Baba Prem Prakash <152669128+premcodeexplorer@users.noreply.github.com>
2024-09-22 06:54:28 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const weatherstackAPIKey = process.env.WEATHERSTACK_API_KEY;
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
