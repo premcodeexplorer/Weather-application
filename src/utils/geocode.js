41f8910f9555fd1c55e2ab7c407ef097d91e7de7
Baba Prem Prakash <152669128+premcodeexplorer@users.noreply.github.com>
2024-09-22 08:10:03 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,18 +1,21 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/search/geocode/v6/forward?q=' + address + '&access_token=' + process.env.MAPBOX_API_KEY + '&limit=1'
+    const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(address)}&access_token=${process.env.MAPBOX_API_KEY}&limit=1`
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
+            console.error('Location service connection error:', error);
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (!body.features || body.features.length === 0) {
+            console.error('Location not found for address:', address);
             callback('Unable to find location. Try another search.', undefined)
         } else {
+            const feature = body.features[0];
             callback(undefined, {
-                latitude: body.features[0].geometry.coordinates[1],
-               longitude: body.features[0].geometry.coordinates[0],
-               location: body.features[0].properties.name
+                latitude: feature.geometry.coordinates[1],
+                longitude: feature.geometry.coordinates[0],
+                location: feature.properties.name
             })
         }
     })
